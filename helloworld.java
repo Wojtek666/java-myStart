fc3fcc80e27523b71ad9531c8d20250d1cd7460d
Wojciech Dziamski <wojtekdziamski@MacBook-Pro-Wojciech.local>
2019-11-30 18:38:01 +0100

    test+array test

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld{
   public static void main(String[] args){
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda","deawoo","bus musi latac"};
     for(String i : cars){
       System.out.println(i);
     }
